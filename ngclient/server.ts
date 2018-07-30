8b7eff5ee2d59758e26f586779e4bbd963b1386a
Stanley Luck <lucksd@gmail.com>
2018-07-30 12:37:40 -0400

    Angular 6 updates

--- ngclient/server.ts
+++ ngclient/server.ts
@@ -1,13 +1,13 @@
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
-import { renderModuleFactory } from '@angular/platform-server';
-import { enableProdMode } from '@angular/core';
+import {enableProdMode} from '@angular/core';
+// Express Engine
+import {ngExpressEngine} from '@nguniversal/express-engine';
+// Import module map for lazy loading
+import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
-import * as request from 'request';
-
-import { join } from 'path';
-import { readFileSync } from 'fs';
+import {join} from 'path';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -18,16 +18,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
-// Our index.html we'll use as our template
-const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
-
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
-
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
@@ -40,24 +32,15 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
-/* - Example Express Rest API endpoints -
-  app.get('/api/**', (req, res) => { });
-*/
-const FlaskRESTapi_host = 'http://localhost:5000';
-app.get('/api/**', (req, res) => {
-  request({uri: FlaskRESTapi_host + req.originalUrl}, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred and handle it
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    res.send(body);
-  });
-});
+// Example Express Rest API endpoints
+// app.get('/api/**', (req, res) => { });
 
 // Server static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
   maxAge: '1y'
 }));
 
-// ALl regular routes use the Universal engine
+// All regular routes use the Universal engine
 app.get('*', (req, res) => {
   res.render('index', { req });
 });
--- ngclient/webpack.server.config.js
+++ ngclient/webpack.server.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'none',
   entry: {
     // This is our Express server for Dynamic universal
     server: './server.ts',
@@ -12,8 +13,9 @@ module.exports = {
   },
   target: 'node',
   resolve: { extensions: ['.ts', '.js'] },
-  // Make sure we include all node_modules etc
-  externals: [/(node_modules|main\..*\.js)/,],
+  optimization: {
+    minimize: false
+  },
   output: {
     // Puts the output at the root of the dist folder
     path: path.join(__dirname, 'dist'),
@@ -21,7 +23,13 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.ts$/, loader: 'ts-loader' }
+      { test: /\.ts$/, loader: 'ts-loader' },
+      {
+        // Mark files inside `@angular/core` as using SystemJS style dynamic imports.
+        // Removing this will cause deprecation warnings to appear.
+        test: /(\\|\/)@angular(\\|\/)core(\\|\/).+\.js$/,
+        parser: { system: true },
+      },
     ]
   },
   plugins: [
@@ -39,4 +47,3 @@ module.exports = {
     )
   ]
 }
-  
