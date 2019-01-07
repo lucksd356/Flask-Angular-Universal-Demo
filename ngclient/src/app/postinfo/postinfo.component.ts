bd581a1a6bd2cad878eb328c5c66410b06f8c226
Stanley Luck <lucksd@gmail.com>
2019-01-06 20:03:47 -0500

    Angular 7 update

--- ngclient/src/app/postinfo/postinfo.component.ts
+++ ngclient/src/app/postinfo/postinfo.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import {Observable} from "rxjs/Observable";
 import { HttpClient } from '@angular/common/http';
 
 @Component({
