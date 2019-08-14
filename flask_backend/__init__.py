d2a707d408af1cb3f878cf8dd7dbbc8502b39b9b
Stanley Luck <lucksd@gmail.com>
2019-08-14 09:18:56 -0400

    Update __init__.py

--- flask_backend/__init__.py
+++ flask_backend/__init__.py
@@ -1,12 +1,7 @@
-import os
-import json
-from flask import Flask, request, Response
-from flask import render_template, send_from_directory, url_for
+from flask import Flask
 
 app = Flask(__name__)
-
 app.config.from_object('flask_backend.settings')
-
 app.url_map.strict_slashes = False
 
 import flask_backend.core
