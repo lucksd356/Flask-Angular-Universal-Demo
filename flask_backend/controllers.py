1080816ea571fec2101d531dc340eef6d24e6e6c
Stanley Luck <lucksd@gmail.com>
2019-08-14 09:24:10 -0400

    Update controllers.py

--- flask_backend/controllers.py
+++ flask_backend/controllers.py
@@ -1,19 +1,7 @@
 import os
-
-from flask import Flask, request, Response, json
-from flask import render_template, url_for, redirect, send_from_directory
-from flask import send_file, make_response, abort
-
+from flask import render_template, make_response, send_from_directory
 from flask_backend import app
-
-# routing for API endpoints, generated from the models designated as API_MODELS
-from flask_backend.core import api_manager
-from flask_backend.models import *
-
-for model_name in app.config['API_MODELS']:
-    model_class = app.config['API_MODELS'][model_name]
-    api_manager.create_api(model_class, methods=['GET', 'POST'], url_prefix='/dbapi')
-session = api_manager.session
+# from flask import send_file, abort, url_for, redirect
 
 
 # routing for basic pages (pass routing onto the Angular app)
@@ -22,22 +10,6 @@ session = api_manager.session
 @app.route('/blog')
 def basic_pages(**kwargs):
     return make_response(open('flask_backend/templates/index.html').read())
-
-
-# routing for CRUD-style endpoints
-# passes routing onto the angular frontend if the requested resource exists
-from sqlalchemy.sql import exists
-crud_url_models = app.config['CRUD_URL_MODELS']
-@app.route('/<model_name>/')
-@app.route('/<model_name>/<item_id>')
-def rest_pages(model_name, item_id=None):
-    if model_name in crud_url_models:
-        model_class = crud_url_models[model_name]
-        if item_id is None or session.query(exists().where(
-                model_class.id == item_id)).scalar():
-            return make_response(open(
-                'flask_backend/templates/index.html').read())
-    abort(404)
 
 
 # special file handlers and error handlers
